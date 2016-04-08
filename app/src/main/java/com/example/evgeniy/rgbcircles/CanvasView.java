d0400fc3471d298db6f3d4ada402d87af55a010f
empoluboyarov <poluboyarovem@gmail.com>
2016-04-08 22:18:44 +0300

    Version 0.09

--- app/src/main/java/com/example/evgeniy/rgbcircles/CanvasView.java
+++ app/src/main/java/com/example/evgeniy/rgbcircles/CanvasView.java
@@ -54,6 +54,7 @@ public class CanvasView extends View implements ICanvasView {
 
     @Override
     public void drawCircle(SimpleCircle circle) {
+        paint.setColor(circle.getColor());
         canvas.drawCircle(circle.getX(),circle.getY(),circle.getRadius(),paint);
     }
 
--- app/src/main/java/com/example/evgeniy/rgbcircles/SimpleCircle.java
+++ app/src/main/java/com/example/evgeniy/rgbcircles/SimpleCircle.java
@@ -7,6 +7,7 @@ public class SimpleCircle {
     protected int x;
     protected int y;
     protected int radius;
+    private int color;
 
     public SimpleCircle(int x, int y, int radius) {
         this.x = x;
@@ -26,4 +27,11 @@ public class SimpleCircle {
         return y;
     }
 
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
 }
